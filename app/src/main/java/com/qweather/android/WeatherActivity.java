c05e1de34af39364d2908aac5699fae2d83b36ed
fyj0846 <qiujian0846@163.com>
2018-03-29 21:51:36 +0800

    定时刷新天气和图片

--- app/src/main/java/com/qweather/android/WeatherActivity.java
+++ app/src/main/java/com/qweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.qweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -25,6 +26,7 @@ import com.qweather.android.gson.ForecastDay;
 import com.qweather.android.gson.ForecastHour;
 import com.qweather.android.gson.Lifestyle;
 import com.qweather.android.gson.Weather;
+import com.qweather.android.service.AutoUpdateService;
 import com.qweather.android.util.HttpUtil;
 import com.qweather.android.util.Utility;
 
@@ -67,7 +69,8 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         if(Build.VERSION.SDK_INT >= 21) {
             View decorView = getWindow().getDecorView();
-            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
 
@@ -102,6 +105,7 @@ public class WeatherActivity extends AppCompatActivity {
             // 缓存天气信息
             Log.d(TAG, "onCreate: " + weatherString);
             Weather weather = Utility.handleWeatherResponse(weatherString);
+            mWeatherId = weather.basic.weatherId;
             showWeatherInfo(weather);
         } else {
             // 无天气缓存信息
@@ -233,6 +237,10 @@ public class WeatherActivity extends AppCompatActivity {
             }
         }
         weatherLayout.setVisibility(View.VISIBLE);
+
+        // 启动定时任务
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     public void loadBingPic() {
